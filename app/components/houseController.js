c664ebd82bbde4829a4f3fe36522ff793d741ae2
Caleb Sherfick <calebsherfick@gmail.com>
2019-02-13 15:32:56 -0700

    completed coding the controller

--- app/components/houseController.js
+++ app/components/houseController.js
@@ -1,13 +1,48 @@
 import HouseService from "./houseService.js";
 
-
 //PRIVATE
-let _hs = new HouseService
+let _hs = new HouseService()
 
 
+function draw() {
+  let houses = _hs.Houses
+  let houseTemplate = ''
+  houses.forEach(house => {
+    houseTemplate += house.getHouseTemplate()
+  });
+  document.getElementById('available-houses').innerHTML = houseTemplate
+}
 
+function logHouses() {
+  console.log("houses UPDATED!!!")
+}
 
 //PUBLIC
 export default class HouseController {
+  constructor() {
+    _hs.addSubscriber('houses', draw)
+    _hs.addSubscriber('houses', logHouses)
+    draw()
+  }
 
+
+
+  addHouse(event) {
+    event.preventDefault();
+    let form = event.target
+    let newHouse = {
+      title: form.title.value,
+      price: form.price.value,
+      description: form.description.value,
+      img: form.img.value
+    }
+
+    _hs.addHouse(newHouse)
+
+    form.reset()
+
+  }
+  deleteHouse(houseId) {
+    _hs.deleteHouse(houseId)
+  }
 }
